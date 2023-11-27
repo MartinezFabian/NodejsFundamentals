5216d5644dcd4669aa9773f532226b520ca894cc
MartinezFabian <FabianMartinezDev@gmail.com>
2023-11-27 19:51:49 -0300

    Add 04_CommonJSModules

---
+++ 04_CommonJSModules/app.js
@@ -0,0 +1,14 @@
+const myFunction = require('./myModule');
+
+console.log(myFunction());
+
+const myData = require('./myData');
+
+console.log(myData);
+
+const { myWebAddress, myNumber, myArray, userData } = require('./myData');
+
+console.log(myWebAddress);
+console.log(myNumber);
+console.log(myArray);
+console.log(userData);
---
+++ 04_CommonJSModules/myData.js
@@ -0,0 +1,28 @@
+const myWebAddress = 'fabian.com';
+
+const myNumber = 22;
+
+const myArray = [10, 20, 30];
+
+const userData = {
+  username: 'fabian10',
+  fullName: 'Fabian Martinez',
+};
+
+module.exports = {
+  myWebAddress,
+  myNumber,
+  myArray,
+  userData,
+};
+
+/* 
+
+module.exports = {
+  myWebAddress: myWebAddress,
+  myNumber: myNumber,
+  myArray: myArray,
+  userData: userData,
+};
+
+*/
