9def170bf2d619f24d60cf7293bb2e6d2530b19c
MartinezFabian <FabianMartinezDev@gmail.com>
2023-11-26 21:41:50 -0300

    Add 02_globals

---
+++ 02_globals/index.js
@@ -0,0 +1,14 @@
+// Imprime el directorio actual del archivo en ejecución.
+console.log(__dirname);
+
+// Imprime la ruta completa del archivo en ejecución.
+console.log(__filename);
+
+// Imprime información sobre el módulo actual.
+console.log(module);
+
+// Imprime la función require() utilizada para importar módulos en Node.js.
+console.log(require);
+
+// Imprime información sobre el proceso en ejecución, como variables de entorno, argumentos de la línea de comandos, etc.
+console.log(process);
